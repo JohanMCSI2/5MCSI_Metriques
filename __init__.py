046be94b0740fda9f2b325f01a00d5fafd205274
JohanMCSI2 <159790208+JohanMCSI2@users.noreply.github.com>
2024-02-12 17:05:21 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,19 +1,19 @@
 from flask import Flask, render_template_string, render_template, jsonify
-#from flask import render_template
+from flask import render_template
 from flask import json
 from datetime import datetime
 from urllib.request import urlopen
 import sqlite3
                                                                                                                                        
-app = Flask(__name__)                                                                                                                  
+app = Flask(_name_)                                                                                                                  
                                                                                                                                        
-@app.route('/')
-def hello_world():
-    return render_template('hello.html') #TassaditYACINE2
-
 @app.route("/contact/")
 def contact():
-    return render_template("Contact.html")
+    return render_template("contact.HTML")
+
+@app.route('/')
+def hello_world():
+    return render_template('hello.html')
 
 @app.route('/paris/')
 def meteo():
@@ -26,6 +26,10 @@ def meteo():
         temp_day_value = list_element.get('temp', {}).get('day') - 273.15 # Conversion de Kelvin en °c 
         results.append({'Jour': dt_value, 'temp': temp_day_value})
     return jsonify(results=results)
+
+@app.route("/rapport/")
+def mongraphique():
+    return render_template("graphique.html")
   
-if __name__ == "__main__":
+if _name_ == "_main_":
   app.run(debug=True)
